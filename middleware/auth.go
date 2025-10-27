6e9b17a3bc521037ab69d47c3b8864672f53ea25
yosaidi <saidiyounes150@gmail.com>
2025-10-27 13:45:06 +0100

    fixed status code

--- middleware/auth.go
+++ middleware/auth.go
@@ -176,3 +176,8 @@ type responseWriter struct {
 	http.ResponseWriter
 	statusCode int
 }
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
+}
